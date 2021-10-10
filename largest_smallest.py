3efa0a9d26bfe4ce7ccc0da03ce33c6cdfa27fc1
michellevrp <92104758+michellevrp@users.noreply.github.com>
2021-10-10 19:04:34 +0200

    Create largest_smallest.py

---
+++ largest_smallest.py
@@ -0,0 +1,10 @@
+
+#fifth exercise
+largest=-1
+print('Before', largest)
+
+for the_num in [5 ,84, 63, 92, 5, 71]:
+    if the_num>largest:
+        largest=the_num
+    print(largest,the_num)
+print('After',largest)
