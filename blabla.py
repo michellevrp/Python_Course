38902ae760a20897c3f71621f0105742149b4e3e
michellevrp <92104758+michellevrp@users.noreply.github.com>
2021-10-10 19:03:44 +0200

    Create blabla.py

---
+++ blabla.py
@@ -0,0 +1,17 @@
+
+#third exercise
+score = input("Enter Score: ")
+try:
+    sc = float(score)
+except:
+    print("Insert numeric input")
+if sc >= 0.9:
+    print("A")
+elif sc >= 0.8:
+    print("B")
+elif sc >= 0.7:
+    print("C")
+elif sc >= 0.6:
+    print("D")
+elif sc<0.6:
+    print("F")
